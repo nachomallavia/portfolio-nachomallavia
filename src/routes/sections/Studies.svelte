ac42ee9538315e0b08688a3feba7f8f0f704103c
nachomallavia <nachomallavia@hotmail.com>
2023-06-22 18:31:13 -0300

    Skills con iconos

---
+++ src/routes/sections/Studies.svelte
@@ -0,0 +1,14 @@
+<section class="full" id="section-why">
+	<h2>Studies</h2>
+	
+
+</section>
+
+<style>
+	.full {
+		width: 100%;
+		min-height: 100%;
+		background-color: var(--background-color-1);
+		color: var(--text-color);
+	}
+</style>
