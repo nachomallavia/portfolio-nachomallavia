8f1071ef12e371fb39bc1cbce3f61188d05f13dd
Nacho Mallavia <nachomallavia@hotmail.com>
2023-09-10 21:10:57 -0300

    Ajustes Coordenadas de impresion

--- src/routes/sections/work/WorkPrintingCoordinates.svelte
+++ src/routes/sections/work/WorkPrintingCoordinates.svelte
@@ -15,7 +15,7 @@
 	let canvasWidth = 180;
 	let canvasHeight = 225;
 
-	let scale = printerWidth / canvasWidth;
+	$: scale = printerWidth / canvasWidth;
 
 	let sizeOptions = [
 		{
@@ -87,6 +87,22 @@
 	let canvas;
 
 	function setupCanvas() {
+		let sectionContainer = document.getElementById('section-coordinates');
+		let canvasProportion = 0.44;
+		let containerWidth = 400;
+		
+		console.log(window.innerWidth)
+		if(window.innerWidth <= 424){			
+			containerWidth = window.innerWidth - 24;
+		}
+		canvasWidth = containerWidth * canvasProportion;
+		canvasHeight = canvasWidth * 1.25;
+		let canvasWidthOffset = (containerWidth / 2) - (canvasWidth / 2);
+		sectionContainer.style.setProperty('--canvas-width-offset', `${canvasWidthOffset*0.975}px`)
+		sectionContainer.style.setProperty('--canvas-height-offest', `${(containerWidth*1.25)*0.32}px`)
+		firstImageWidth = canvasWidth/2;
+		firstImagePosition.left = firstImageWidth/2;
+		
 		canvas = new fabric.Canvas(canvasId, {
 			width: canvasWidth,
 			height: canvasHeight
@@ -129,10 +145,12 @@
 		imageHeight = image.getScaledHeight();
 		imagePosition.x = image.getCoords()[0].x;
 		imagePosition.y = image.getCoords()[0].y;
-		// console.log(imageWidth, imageHeight, imagePosition);
+		
 	}
 	onMount(() => {
-		setupCanvas();
+		if(browser){
+			setupCanvas();
+		}
 	});
 
 	function resetCanvas() {
@@ -170,7 +188,9 @@
 		})
 
 	}
+	
 	if(browser){
+
 		let valueCollection = document.getElementsByClassName('value');
 		let valueArray = [...valueCollection];
 		valueArray.forEach(value=>{
@@ -182,13 +202,33 @@
 	})}
 </script>
 
-<div class="container">
-	<div class="layer-container">
-		<img src={tshirtColor} alt="" class="tshirt-color" draggable="false" />
-		<div class="canvas-position">
-			<canvas bind:this={canvasId} id="canvas" />
+<div id="section-coordinates" class="container" style="--canvas-container-width: 400px">
+	<div class="controls mobile">
+		<button
+			on:click={() => {
+				resetCanvas();
+			}}>{$lang==="ES"?"Restaurar Imagen":"Reset Image"}</button
+		>
+		<!-- <button on:click={()=>{resetCanvas()}}>Change Image</button> -->
+	</div>
+	<div class="data-grid mobile">
+		<span class="title">{$lang === 'ES' ? 'Ancho' : 'Width'}</span>
+		<span class="title">{$lang === 'ES' ? 'Alto' : 'Height'}</span>
+		<span class="title">{$lang === 'ES' ? 'Pos. X' : 'X Pos. '}</span>
+		<span class="title">{$lang === 'ES' ? 'Pos. Y' : 'Y Pos. '}</span>
+		<span class="2-1 value">{calculatedImageWidth} Cms</span>
+		<span class="2-2 value">{calculatedImageHeight} Cms</span>
+		<span class="2-3 value">{calculatedX} Cms</span>
+		<span class="2-4 value">{calculatedY} Cms</span>
+	</div>
+	<div id="canvas-container">
+		<div class="layer-container" id="layer-container">
+			<img src={tshirtColor} alt="" class="tshirt-color" draggable="false" />
+			<div id="canvas-positioner">
+				<canvas bind:this={canvasId} id="canvas" />		
+			</div>
+			<img src={tshirtTexture} alt="" class="tshirt-texture" draggable="false" />
 		</div>
-		<img src={tshirtTexture} alt="" class="tshirt-texture" draggable="false" />
 	</div>
 
 	<div class="description">
@@ -214,17 +254,17 @@
 				<option value={size.proportion}>{size.label}</option>
 			{/each}
 		</select>
-		<div class="data-grid">
+		<div class="data-grid desktop">
 			<span class="title">{$lang === 'ES' ? 'Ancho' : 'Width'}</span>
 			<span class="title">{$lang === 'ES' ? 'Alto' : 'Height'}</span>
-			<span class="title">{$lang === 'ES' ? 'Posición X' : 'X Position '}</span>
-			<span class="title">{$lang === 'ES' ? 'Posición Y' : 'Y Position '}</span>
+			<span class="title">{$lang === 'ES' ? 'Pos. X' : 'X Pos. '}</span>
+			<span class="title">{$lang === 'ES' ? 'Pos. Y' : 'Y Pos. '}</span>
 			<span class="2-1 value">{calculatedImageWidth} Cms</span>
 			<span class="2-2 value">{calculatedImageHeight} Cms</span>
 			<span class="2-3 value">{calculatedX} Cms</span>
 			<span class="2-4 value">{calculatedY} Cms</span>
 		</div>
-		<div class="controls">
+		<div class="controls desktop">
 			<button
 				on:click={() => {
 					resetCanvas();
@@ -233,19 +273,29 @@
 			<!-- <button on:click={()=>{resetCanvas()}}>Change Image</button> -->
 		</div>
 	</div>
+	
 </div>
 
 <style>
+
+	
 	.container {
+
 		margin-top: 4rem;
 		display: flex;
 		gap: 4rem;
 		padding-inline: 4rem;
 		align-items: start;
 	}
-	.description {
-		padding-right: 4rem;
+	#canvas-container{
+		min-width:var(--canvas-container-width);
+		aspect-ratio: 4 / 5;
 	}
+	#canvas-positioner{
+			position: absolute;
+			left:var(--canvas-width-offset);
+			top:var(--canvas-height-offest);
+		}
 	.description > h2 {
 		font-size: 1.75rem;
 		margin-bottom: 1rem;
@@ -256,10 +306,11 @@
 		margin-bottom: 3rem;
 	}
 	.layer-container {
-		min-width: 440px;
-		max-width: 600px;
-		position: relative;
+		width:100%;
+		height:100%;
+		position: relative;	
 	}
+
 	.layer-container > img {
 		width: 100%;
 		position: absolute;
@@ -267,15 +318,12 @@
 		left: 0;
 		pointer-events: none;
 	}
-	.canvas-position {
-		position: absolute;
-		top: 174px;
-		left: 128px;
-	}
+	
 	#canvas {
 		/* background-color: rgb(48, 150, 228); */
-		border: 1px dashed lightgrey;
+		border: 1px dashed lightgrey;	
 	}
+
 	button {
 		font-size: 1rem;
 		padding: 0.5rem 1rem;
@@ -323,5 +371,59 @@
 	.value {
 		font-size: 1.25rem;
 		color:var(--accent-color)
+	}
+	.mobile{
+		display: none;
+	}
+	@media screen and (max-width:512px){
+		.container{
+			flex-direction: column-reverse;
+			padding-inline: 1rem;
+			gap: 1rem;
+			margin-top: 0rem;
+		}
+		#canvas-container{
+			width:100%;
+			min-width:250px;
+			aspect-ratio: 4 / 5;
+		}
+		
+		.layer-container {	
+			width:100%;
+			height:100%;
+			position: relative;		
+			margin-bottom: 4rem;
+		}
+		
+		.description {
+			padding-right: 0rem;
+		}
+		h2 {
+			font-size: 1.5rem;
+			margin-bottom: 2rem;
+		}
+		
+		.description > p {	
+			margin-bottom: 1rem;
+		}
+		
+		.mobile{
+		width: 100%;
+		display: block;
+		}
+		.data-grid {
+		padding-top: 1rem;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		padding-bottom: 2rem;
+		}
+		
+		button {
+		width: 100%;
+		padding-block: .75rem;
+		}
+		.desktop{
+			display: none;
+		}
 	}
 </style>
