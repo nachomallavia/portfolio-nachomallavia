cfa162f37a7c133aaceef12f227ce54a7d75a0bf
nachomallavia <nachomallavia@hotmail.com>
2026-06-07 17:52:04 -0300

    Chau gtag manual

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,15 +12,7 @@
 
 <div class="app">
 	<!-- <Header /> -->
-	 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LZN40DVK4X"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-LZN40DVK4X');
-</script>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
